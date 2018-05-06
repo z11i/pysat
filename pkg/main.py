5c4d1bd00103a3d0e307c3faf610844a2c91aeef
zhongwei zhang <zhangzhongwei.sg@gmail.com>
2018-05-06 22:37:54 +0800

    feat: choose heuristics at main

--- pkg/main.py
+++ pkg/main.py
@@ -3,7 +3,9 @@ SAT solver using CDCL
 """
 
 import argparse
+import os
 from pkg.pysat import solver
+from pkg.pysat import branch_heuristics as solvers
 
 
 if __name__ == '__main__':
@@ -17,6 +19,12 @@ if __name__ == '__main__':
         nargs='?',
         help='path of .cnf file')
     parser.add_argument(
+        'heuristics',
+        type=str,
+        nargs='?',
+        default='RandomChoiceSolver',
+        help='choose heuristics to branch variable: RandomChoiceSolver (default) | FrequentVarsFirstSolver')
+    parser.add_argument(
         '--loglevel',
         default='WARNING',
         nargs='?',
@@ -29,6 +37,6 @@ if __name__ == '__main__':
         exit()
 
     solver.logger.setLevel(args.loglevel)
-    solver = solver.Solver(filename=args.filename)
+    solver = getattr(solvers, args.heuristics)(args.filename)
     _, _, answer = solver.run()
     print(answer)
